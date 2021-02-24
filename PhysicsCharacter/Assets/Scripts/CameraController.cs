ad2ae8e0e4054a67712b5c96b40e0a3212333777
Dylan White <dylanhwite@Gmail.com>
2021-02-24 16:31:25 +1030

    added unity project

---
+++ PhysicsCharacter/Assets/Scripts/CameraController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    float heightOffset = 1.5f;
+    public float rotateSpeed = 180;
+    public float distance = 5;
+    float currentDistance;
+    public float relaxSpeed;
+    public bool invertY;
+
+    Vector3 GetTargetPosition()
+    {
+        return target.position + heightOffset * Vector3.up;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDistance = distance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // right drag rotates the camera
+        if (Input.GetMouseButton(1))
+        {
+            Vector3 angles = transform.eulerAngles;
+
+            float dx;
+            float dy = Input.GetAxis("Mouse X");
+            if (invertY)
+            {
+                dx = Input.GetAxis("Mouse Y");
+            }
+            else
+            {
+                dx = -Input.GetAxis("Mouse Y");
+            }
+
+            // look up and down by rotating around X-axis
+            angles.x = Mathf.Clamp(angles.x + dx * rotateSpeed * Time.deltaTime, 0, 70);
+            // spin the camera round
+            angles.y += dy * rotateSpeed * Time.deltaTime;
+            transform.eulerAngles = angles;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(GetTargetPosition(), -transform.forward, out hit, distance))
+        {
+            // snap the camera right in to where the collision happened
+            currentDistance = hit.distance;
+        }
+        else
+        {
+            // relax the camera back to the desired distance
+            currentDistance = Mathf.MoveTowards(currentDistance, distance, Time.deltaTime * relaxSpeed);
+        }
+
+        // look at the target point
+        transform.position = GetTargetPosition() - currentDistance * transform.forward;
+    }
+}
---
+++ PhysicsCharacter/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float speed = 10;
+    public float jumpForce = 10;
+    public bool isGrounded;
+    private bool jumpInput = false;
+
+    CharacterController cc;
+    
+    Transform cam;
+    Vector2 moveInput = new Vector2();
+    public Vector3 velocity = new Vector3();
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        jumpInput = Input.GetButton("Jump");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        cam = Camera.main.transform;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 delta;
+
+        // find the horizontal unit vector facing forward from the camera
+        Vector3 camForward = cam.forward;
+        camForward.y = 0;
+        camForward.Normalize();
+
+        // use our camera's right vector, which is always horizontal
+        Vector3 camRight = cam.right;
+        delta = (moveInput.x * camRight + moveInput.y * camForward) * speed * Time.fixedDeltaTime;
+
+        if (isGrounded || moveInput.x != 0 || moveInput.y != 0)
+        {
+            velocity.x = delta.x;
+            velocity.z = delta.z;
+        }
+
+        // check for jumping
+        if (jumpInput && isGrounded)
+        {
+            velocity.y = jumpForce;
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0;
+        }
+
+        // apply gravity after zeroing velocity so we register as grounded still
+        velocity += Physics.gravity * Time.fixedDeltaTime;
+
+        // and apply this to our positional update this frame
+        //delta += velocity * Time.deltaTime;
+
+        cc.Move(velocity * Time.deltaTime);
+        isGrounded = cc.isGrounded;
+
+        transform.forward = camForward;
+
+    }
+}
