a68e9338b217dff465cc55300f7cd690d8e35966
Dylan White <dylanhwite@Gmail.com>
2021-04-06 11:33:12 +0930

    More documentation

--- Project2D/PhysicsApp.h
+++ Project2D/PhysicsApp.h
@@ -6,28 +6,77 @@
 
 using namespace aie;
 
-class PhysicsApp : public aie::Application 
+/// <summary>
+/// The application class that allows swapping between scenes
+/// </summary>
+class PhysicsApp : public aie::Application
 {
 public:
-
+	/// <summary>
+	/// The constructor of the physics application
+	/// </summary>
 	PhysicsApp();
+
+	/// <summary>
+	/// The destructor of the physics application
+	/// </summary>
 	virtual ~PhysicsApp();
 
+	/// <summary>
+	/// When the application begins, this function sets the gravity, time step, and starts the ship scene
+	/// </summary>
 	virtual bool startup();
+
+	/// <summary>
+	/// This function deletes the font and renderer
+	/// </summary>
 	virtual void shutdown();
 
+	/// <summary>
+	/// /this function is called every frame
+	/// </summary>
+	/// <param name="deltaTime"></param>
 	virtual void update(float deltaTime);
+
+	/// <summary>
+	/// 
+	/// </summary>
 	virtual void draw();
+
+	/// <summary>
+	/// 
+	/// </summary>
 	void createShip(vec2 position, float orientation, vec4 colour, EInputCodes inputLeft, EInputCodes inputRight, EInputCodes boostInput);
 
+	/// <summary>
+	/// 
+	/// </summary>
 	void shipScene();
+
+	/// <summary>
+	/// 
+	/// </summary>
 	void sandboxScene();
 
 protected:
 
+	/// <summary>
+	/// 
+	/// </summary>
 	aie::Renderer2D* m_2dRenderer;
+
+	/// <summary>
+	/// 
+	/// </summary>
 	aie::Font* m_font;
+
+	/// <summary>
+	/// 
+	/// </summary>
 	PhysicsScene* m_physicsScene;
 
+	/// <summary>
+	/// 
+	/// </summary>
 	float m_timer;
 };
